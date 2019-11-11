0f9c598aa35067d04528a8c3b8c54f7e39e4fa16
Paul Lazunko <paul.lazunko@jibjibstudio.com>
2019-11-11 17:20:39 +0700

    removed commented code block from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-// import { MessageBrokerServer } from './classes/MessageBrokerServer'
-//
-// const server = new MessageBrokerServer({ host: '127.0.0.1', port: 8888, syncInterval: 1000, eventEmitTimeoutValue: 10000  });
-
 export { MessageBrokerServer, MessageBrokerClient, MessageBrokerLoadBalancer } from './classes'
 export { IClientOptions, IClientRequestParams, IServerOptions, ILoadBalancerOptions } from './interfaces'
